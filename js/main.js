99082c7dfad74321612a1d4440cd47dc6da47e81
Chris408-hub <112998909+Chris408-hub@users.noreply.github.com>
2023-02-13 11:33:43 -0500

    adding desktop

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// let userTestimonial = document.querySelector('testimonial'),
+//     buyNow = document.querySelectorAll('.buynow');
+
+// userTestimonial.addEventListener('mouseover', function () {
+//     buyNow.style.display = block;
+//  })  
